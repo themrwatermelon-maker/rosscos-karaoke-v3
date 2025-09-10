14ebb387543442572040a8dcb683e7e7ddc765ef
themrwatermelon-maker <themrwatermelon@gmail.com>
2025-09-10 13:24:54 -0600

    Update firebase-config.js

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,15 +1,24 @@
 // scripts/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDnttfMZEGYfXksxjvWJWTgPkJCgeyglOA",
+    authDomain: "rosscos-karaoke.firebaseapp.com",
+    projectId: "rosscos-karaoke",
+    storageBucket: "rosscos-karaoke.firebasestorage.app",
+    messagingSenderId: "838114183982",
+    appId: "1:838114183982:web:ca4916a95c16476b6a08bf",
+    measurementId: "G-4SL232YLXC"
+  };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
